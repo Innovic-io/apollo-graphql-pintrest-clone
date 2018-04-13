c8b5b5f262d4381ec13eed376d11a6a83b2e78a1
vladimir <vladimir.kunarac@gmail.com>
2018-04-13 09:10:36 +0200

    decorator start

--- src/graphql/boards/board.resolver.ts
+++ src/graphql/boards/board.resolver.ts
@@ -8,100 +8,116 @@ import { SERVICE_TYPES } from '../../inversify/inversify.types';
 
 let boardService;
 
-@injectable()
+let queries = {};
+let mutations = {};
+let board = {
+
+  async followers(boards: IBoard) {
+
+    if (!boards.followers) {
+      return null;
+    }
+
+    return await boardService.getUsers(boards._id, boards.followers);
+  },
+
+  async creator(boards: IBoard) {
+
+    return await getServiceById(boards.creator, SERVICE_ENUM.USERS);
+  },
+
+  async collaborators(boards: IBoard) {
+
+    if (!boards.collaborators) {
+      return null;
+    }
+
+    return await boardService.getUsers(boards._id, boards.collaborators);
+  },
+};
+
+const resolverQueries = {
+  Query: {},
+  Mutation: {},
+  Subscription: {},
+};
+
+function Resolver(typeToResolve: string) {
+  resolverQueries[typeToResolve] = board;
+  return (target) => {
+
+    const types = Reflect.getMetadata('design:paramtypes', target) || [];
+    Reflect.defineMetadata('inversify:paramtypes', types, target);
+
+    target.prototype.getAll = () => resolverQueries;
+
+    return target;
+  };
+}
+
+function Query() {
+
+  return (target, propertyKey: string, descriptor: PropertyDescriptor) => Object.assign(
+    resolverQueries.Query,
+    {[ propertyKey ]: descriptor.value});
+}
+
+function Mutation() {
+
+  return (target, propertyKey: string, descriptor: PropertyDescriptor) => Object.assign(
+    resolverQueries.Mutation,
+    {[ propertyKey ]: descriptor.value});
+}
+
+@Resolver('Board')
+// @injectable()
 export default class BoardResolver implements IBoardResolver {
-  private Query;
-  private Mutation;
-  private Board;
 
-  constructor(
-    @inject(SERVICE_TYPES.BoardService) injectedBoardService: IBoardService,
-  ) {
-
-    this.setQuery();
-    this.setMutation();
-    this.setBoard();
+  constructor( @inject(SERVICE_TYPES.BoardService) injectedBoardService: IBoardService ) {
 
     boardService = injectedBoardService;
   }
 
-  setQuery() {
-    this.Query = {
-      async getBoard(parent, { _id }) {
-        return await boardService.getByID(_id);
-      },
-
-      async getBoardFollowing(parent, args, context: IAuthorization) {
-
-        return await boardService.getBoardFollowing(context._id);
-      },
-      async getUserBoards(parent, args, context: IAuthorization) {
-
-        return await boardService.getCreators(context._id);
-      },
-    };
-    return this;
+  @Query()
+  async getBoard(parent, { _id }) {
+    return await boardService.getByID(_id);
   }
 
-  setMutation() {
-    this.Mutation = {
-      async createBoard(parent, args, context: IAuthorization) {
-        return await boardService.createBoard(args, context._id);
-      },
+  @Query()
+  async getBoardFollowing(parent, args, context: IAuthorization) {
 
-      async updateBoard(parent, args, context: IAuthorization) {
-        return await boardService.updateBoard(args, context._id);
-      },
-
-      async deleteBoard(parent, { _id }, context: IAuthorization) {
-        return await boardService.deleteBoard(_id, context._id);
-      },
-
-      async followBoard(parent, { _id }, context: IAuthorization) {
-        return await boardService.startFollowingBoard(_id, context._id);
-      },
-
-      async stopFollowingBoard(parent, { _id }, context: IAuthorization) {
-        return await boardService.stopFollowingBoard(_id, context._id);
-      },
-    };
-    return this;
+    return await boardService.getBoardFollowing(context._id);
   }
 
-  setBoard() {
-    this.Board = {
-
-      async followers(boards: IBoard) {
-
-        if (!boards.followers) {
-          return null;
-        }
-
-        return await boardService.getUsers(boards._id, boards.followers);
-      },
-
-      async creator(boards: IBoard) {
-
-        return await getServiceById(boards.creator, SERVICE_ENUM.USERS);
-      },
-
-      async collaborators(boards: IBoard) {
-
-        if (!boards.collaborators) {
-          return null;
-        }
-
-        return await boardService.getUsers(boards._id, boards.collaborators);
-      },
-    };
-    return this;
+  @Query()
+  async getUserBoards(parent, args, context: IAuthorization) {
+    return await boardService.getCreators(context._id);
   }
 
-  getAll() {
-    return {
-      Query: this.Query,
-      Mutation: this.Mutation,
-      Board: this.Board,
-    };
+  @Mutation()
+  async createBoard(parent, args, context: IAuthorization) {
+    return await boardService.createBoard(args, context._id);
   }
+
+  @Mutation()
+  async updateBoard(parent, args, context: IAuthorization) {
+    return await boardService.updateBoard(args, context._id);
+  }
+
+  @Mutation()
+  async deleteBoard(parent, { _id }, context: IAuthorization) {
+    return await boardService.deleteBoard(_id, context._id);
+  }
+
+  @Mutation()
+  async followBoard(parent, { _id }, context: IAuthorization) {
+    return await boardService.startFollowingBoard(_id, context._id);
+  }
+
+  @Mutation()
+  async stopFollowingBoard(parent, { _id }, context: IAuthorization) {
+    return await boardService.stopFollowingBoard(_id, context._id);
+  }
+
+  getAll() {}
 }
